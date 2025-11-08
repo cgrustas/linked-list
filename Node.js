21ffb77a097ff4bbafa5fa8feeb4dd613c11edb4
Coleman Grustas <grustas.c@northeastern.edu>
2025-11-08 10:19:11 -0500

    Create Node class structure

---
+++ Node.js
@@ -0,0 +1,26 @@
+/**
+ * Represents a node in a linked list
+ */
+class Node {
+  #value;
+  #nextNode;
+
+  constructor() {
+    this.#value = null;
+    this.#nextNode = null;
+  }
+
+  /**
+   * @returns a single element of data
+   */
+  get value() {
+    return this.#value;
+  }
+
+  /**
+   * @returns pointer to the next node in the list
+   */
+  get nextNode() {
+    return this.#nextNode;
+  }
+}

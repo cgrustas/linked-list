c0d35686a6d8a14bbef68b038709997d61f3e4ca
Coleman Grustas <grustas.c@northeastern.edu>
2025-11-08 11:54:47 -0500

    Add methods to manipulate LinkedList
    Adds the following methods:
    - append(value) adds a new node containing value to the end of the list
    - prepend(value) adds a new node containing value to the start of the
      list
    - size returns the total number of nodes in the list
    - head returns the first node in the list
    - tail returns the last node in the list
    - at(index) returns the node at the given index
    - pop removes the last element from the list
    - contains(value) returns true if the passed in value is in the list and
      otherwise returns false.
    - find(value) returns the index of the node containing value, or null if
      not found.
    - toString represents your LinkedList objects as strings

--- LinkedList.js
+++ LinkedList.js
@@ -1,3 +1,5 @@
+import { Node } from "./Node.js";
+
 /**
  * Represents a linked list
  */
@@ -6,5 +8,136 @@ export class LinkedList {
 
   constructor() {
     this.#head = null;
+  }
+
+  /**
+   * Adds a new node containing the given value to the end of the list
+   * @param {*} value - a single element of data to store in the new node
+   * @returns {void}
+   */
+  append(value) {
+    if (this.#head === null) {
+      this.#head = new Node(value, null);
+    } else {
+      let temp = this.#head;
+      while (temp.nextNode !== null) {
+        temp = temp.nextNode;
+      }
+      temp.nextNode = new Node(value, null);
+    }
+  }
+
+  /**
+   * Adds a new node containing the given value to the start of the list
+   * @param {*} value - a single element of data to store in the new node
+   * @returns {void}
+   */
+  prepend(value) {
+    this.#head = new Node(value, this.#head);
+  }
+
+  /**
+   * Traverses the list, and returns the total number of nodes in the list
+   * @returns {Number} linked list size
+   */
+  size() {
+    let size = 0;
+    let temp = this.#head;
+    while (temp !== null) {
+      size++;
+      temp = temp.nextNode;
+    }
+    return size;
+  }
+
+  /**
+   * Returns the first node in the list
+   * @returns {Node|null} first node in list, null if empty
+   */
+  head() {
+    return this.#head;
+  }
+
+  /**
+   * Returns the last node in the list
+   * @returns {Node|null} first node in list, null if empty
+   */
+  tail() {
+    if (this.#head === null) return this.#head;
+
+    let temp = this.#head;
+    while (temp.nextNode !== null) {
+      temp = temp.nextNode;
+    }
+    return temp;
+  }
+
+  /**
+   * Removes the last element from the list
+   * @returns {void}
+   */
+  pop() {
+    if (this.#head === null) return;
+
+    let prev = this.#head;
+    if (prev.nextNode === null) {
+      this.#head = null;
+      return;
+    }
+
+    let curr = prev.nextNode;
+    while (curr.nextNode !== null) {
+      prev = prev.nextNode;
+      curr = curr.nextNode;
+    }
+    prev.nextNode = null;
+  }
+
+  /**
+   * Checks if the passed in value is in the list
+   * @param {*} value - the element of data to be found in the list
+   * @returns true if the passed in value is in the list, otherwise false
+   */
+  contains(value) {
+    if (this.#head === null) return false;
+    let temp = this.#head;
+    while (temp !== null) {
+      if (temp.value === value) return true;
+      temp = temp.nextNode;
+    }
+    return false;
+  }
+
+  /**
+   * Finds the index of the node containing the value in the list
+   * @param {*} value - the element of data to be found
+   * @returns {Number|null} the index of the node containing value, or null if not found
+   */
+  find(value) {
+    if (this.#head === null) return null;
+
+    let temp = this.#head;
+    let index = 0;
+    while (temp !== null) {
+      if (temp.value === value) return index;
+      temp = temp.nextNode;
+      index++;
+    }
+    return null;
+  }
+
+  /**
+   * Represents LinkedList objects as strings
+   * @returns {string} formatted LinkedList (e.g. ( value ) -> ( value ) -> ( value ) -> null)
+   */
+  toString() {
+    let string = "";
+    let temp = this.#head;
+    while (temp !== null) {
+      string += `( ${temp.value} ) -> `;
+      temp = temp.nextNode;
+    }
+    string += "null";
+    return string;
   }
 }
--- Node.js
+++ Node.js
@@ -2,25 +2,11 @@
  * Represents a node in a linked list
  */
 export class Node {
-  #value;
-  #nextNode;
+  value;
+  nextNode;
 
-  constructor() {
-    this.#value = null;
-    this.#nextNode = null;
-  }
-
-  /**
-   * @returns a single element of data
-   */
-  get value() {
-    return this.#value;
-  }
-
-  /**
-   * @returns pointer to the next node in the list
-   */
-  get nextNode() {
-    return this.#nextNode;
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
   }
 }
